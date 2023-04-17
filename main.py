b5ccb38cb8e31eea180f442aaed75bb99dd80e4d
sirkotal <lordkotal2002@gmail.com>
2023-04-17 14:04:07 +0100

    Imported the Flask and OpenAI packages

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import openai
+import flask
 
+app = flask.Flask(__name__)
+api_key = os.getenv("OPENAI_API_KEY")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.route("/", methods=("GET", "POST"))
